994f4494dc1aa1d1fce72239363d01d5a9dbe900
Aya Mahmoud <aya122972@gmail.com>
2026-04-21 20:52:02 +0200

    fix deploy

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { Provider } from "react-redux";
  const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+<BrowserRouter basename="/WeatherApp">
     <Provider store={store}>
       <App />
       </Provider>
